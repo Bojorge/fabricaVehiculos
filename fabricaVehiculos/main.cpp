1520ccc407bd59d7174156191b9106f0ee12dd75
Bojorge <38023924+Bojorge@users.noreply.github.com>
2019-04-24 14:33:47 -0600

    Nueva version adaptada al problema

--- fabricaVehiculos/main.cpp
+++ fabricaVehiculos/main.cpp
@@ -1,29 +1,198 @@
 //ROUND ROBIN//
+#include <pthread.h>
 #include<iostream>
-//#include"conio.h"
+
 using namespace std;
 
-void SearchStack01(int pnt,int tm);
-void SearchStack02(int pnt, int tm);
-void AddQue(int pnt);
+//void SearchStack01(int pnt,int tm);
+//void SearchStack02(int pnt, int tm);
+//void roundRobin();
+//void AddQue(int pnt);
+
+class VehiculoTipo1{
+public:
+    int tiempoProcesoA(){
+        return 2;
+    }
+    int tiempoProcesoB(){
+        return 2;
+    }
+    int tiempoProcesoC(){
+        return 4;
+    }
+    int tiempoProcesoD(){
+        return 1;
+    }
+    int tiempoProcesoE(){
+        return 3;
+    }
+    int tiempoProcesoF(){
+        return 1;
+    }
+};
+
+class VehiculoTipo2{
+public:
+    int tiempoProcesoC(){
+        return 2;
+    }
+    int tiempoProcesoA(){
+        return 1;
+    }
+    int tiempoProcesoB(){
+        return 1;
+    }
+    int tiempoProcesoE(){
+        return 2;
+    }
+    int tiempoProcesoD(){
+        return 3;
+    }
+    int tiempoProcesoF(){
+        return 1;
+    }
+};
+
+class VehiculoTipo3{
+public:
+    int tiempoProcesoD(){
+        return 1;
+    }
+    int tiempoProcesoE(){
+        return 4;
+    }
+    int tiempoProcesoA(){
+        return 3;
+    }
+    int tiempoProcesoC(){
+        return 2;
+    }
+    int tiempoProcesoB(){
+        return 1;
+    }
+    int tiempoProcesoF(){
+        return 1;
+    }
+};
+
+class VehiculoTipo4{
+public:
+    int tiempoProcesoB(){
+        return 2;
+    }
+    int tiempoProcesoC(){
+        return 1;
+    }
+    int tiempoProcesoD(){
+        return 4;
+    }
+    int tiempoProcesoA(){
+        return 3;
+    }
+    int tiempoProcesoF(){
+        return 2;
+    }
+    int tiempoProcesoE(){
+        return 1;
+    }
+};
+
+class VehiculoTipo5{
+public:
+    int tiempoProcesoE(){
+        return 4;
+    }
+    int tiempoProcesoF(){
+        return 1;
+    }
+    int tiempoProcesoB(){
+        return 2;
+    }
+    int tiempoProcesoC(){
+        return 4;
+    }
+    int tiempoProcesoA(){
+        return 1;
+    }
+    int tiempoProcesoD(){
+        return 3;
+    }
+};
+
+class VehiculoTipo6{
+public:
+    int tiempoProcesoF(){
+        return 1;
+    }
+    int tiempoProcesoD(){
+        return 1;
+    }
+    int tiempoProcesoC(){
+        return 2;
+    }
+    int tiempoProcesoB(){
+        return 4;
+    }
+    int tiempoProcesoE(){
+        return 3;
+    }
+    int tiempoProcesoA(){
+        return 4;
+    }
+};
+
 int at[50], bt[50], ct[50]={0}, qt, rqi[50]={0}, c=0, st, flg=0, tm=0, noe=0, pnt=0, btm[50]={0}, tt, wt;
 float att, awt;
+VehiculoTipo1 vehiculoTipo1;
+VehiculoTipo2 vehiculoTipo2;
+VehiculoTipo3 vehiculoTipo3;
+VehiculoTipo4 vehiculoTipo4;
+VehiculoTipo5 vehiculoTipo5;
+VehiculoTipo6 vehiculoTipo6;
 
-int main()
-{
-    cout<<"ALGORITMO ROUND ROBIN : PARA 6 PROCESOS\n";
+
+void SearchStack01(int pnt,int tm){
+    for(int x=pnt+1;x<2;x++){
+        if(at[x]<=tm){
+            rqi[noe]=x+1;
+            noe++;}
+    }
+}
+
+void SearchStack02(int pnt, int tm){
+    for(int x=pnt+1;x<2;x++){
+        //---CheckQue
+        int fl=0;
+        for(int y=0;y<noe;y++){
+            if(rqi[y]==x+1){
+                fl++;}}
+        if(at[x]<=tm && fl==0 && btm[x]!=0){
+            rqi[noe]=x+1;
+            noe++;}
+    }
+}
+
+void AddQue(int pnt){
+    rqi[noe]=pnt+1;
+    noe++;
+}
+
+
+void *roundRobin(void *vacio){
+    cout<<"\n ALGORITMO ROUND ROBIN : PARA 2 PROCESOS\n";
     cout<<"\n\t\t Aquí algunos atributos usados en el programa \n";
     cout<<"AT = Tiempo de llegada \nBT = Tiempo total de CPU \nCT = Tiempo de finalizacion \nTT = Tiempo de respuesta \nWT = Tiempo de espera \n\n";
-    for(int x=0;x<6;x++){
+    for(int x=0;x<2;x++){
         cout<<"\nProceso "<<x+1;
-        cout<<"\nTiempo de llegada = ";
+        cout<<"\nTiempo de llegada en el que se inicia el proceso = ";
         cin>>at[x];
-        cout << "Tiempo total de CPU = ";
+        cout << "Tiempo total de CPU que se requiere para terminar el proceso = ";
         cin>>bt[x];
         btm[x]=bt[x];}
-    cout<<"\n Ingrese el quantum : ";
-    cin>>qt;
-    //system("CLS");
+    //cout<<"\n Ingrese el quantum : ";
+    //cin>>qt;
+    qt=1;
+
     cout<<endl<<"Diagrama GANTT"<<endl<<at[0];
     do{
         if(flg==0){
@@ -38,7 +207,7 @@ int main()
                 tm=st+qt;
                 SearchStack01(pnt,tm);
                 AddQue(pnt);}
-        }//if
+        }
         else{
             pnt=rqi[0]-1;
             st=tm;
@@ -56,45 +225,51 @@ int main()
                 tm=st+qt;
                 SearchStack02(pnt, tm);
                 AddQue(pnt);}
-        }//else
+        }
         //AssignCTvalue
         if(btm[pnt]==0){
             ct[pnt]=tm;
-        }//if
+        }
         flg++;1;
         cout<<"]-P"<<pnt+1<<"-["<<tm;
     }while(noe!=0);1;
     cout<<"\n\nPROCESO \t  Tiempo de llegada \t  Tiempo total de CPU \t  Tiempo de finalizacion \t  Tiempo de respuesta \t  Tiempo de espera \n";
-    for(int x=0;x<6;x++){
+    for(int x=0;x<2;x++){
         tt=ct[x]-at[x];
         wt=tt-bt[x];
         cout<<"  P "<<x+1<<"                 "<<at[x]<<"                     "<<bt[x]<<"                          "<<ct[x]<<"                        "<<tt<<"                       "<<wt<<"\n";
         awt=awt+wt;
         att=att+tt;
-    }//for
-    cout<<"\n Promedio de finalizacion : "<<att/5<<"\n Promedio de espera : "<<awt/5;
-}//main
-void SearchStack01(int pnt,int tm){
-    for(int x=pnt+1;x<6;x++){
-        if(at[x]<=tm){
-            rqi[noe]=x+1;
-            noe++;}
-    }//for
-}//void
-void SearchStack02(int pnt, int tm){
-    for(int x=pnt+1;x<6;x++){
-        //---CheckQue
-        int fl=0;
-        for(int y=0;y<noe;y++){
-            if(rqi[y]==x+1){
-                fl++;}}
-        if(at[x]<=tm && fl==0 && btm[x]!=0){
-            rqi[noe]=x+1;
-            noe++;}
-    }//for
-}//void
-void AddQue(int pnt){
-    rqi[noe]=pnt+1;
-    noe++;
-}//void
+    }
+    cout<<"\n Promedio del tiempo de respuesta : "<<att/2<<"\n Promedio de espera : "<<awt/2;
+}
+
+void procesosSimultaneosPorLinea(){
+    void * vacio;
+    pthread_t hilo1;
+    pthread_create(&hilo1, NULL,&roundRobin,vacio);
+    pthread_join(hilo1, NULL);
+
+    pthread_t hilo2;
+    pthread_create(&hilo2, NULL,&roundRobin,vacio);
+    pthread_join(hilo2, NULL);
+
+    pthread_t hilo3;
+    pthread_create(&hilo3, NULL,&roundRobin,vacio);
+    pthread_join(hilo3, NULL);
+
+}
+
+int main()
+{
+    //error al ejecutar este metodo
+    procesosSimultaneosPorLinea();
+
+}
+
+
+
+
+
+
 
